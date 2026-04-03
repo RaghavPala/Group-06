04cd22ef47cfd9bf88171a879f2487956b99430f
ani-pur <anirudhpurohit1101@gmail.com>
2026-04-03 15:35:17 -0500

    Phase 4 (#1)
    * Log in, log out, access dashboard screens
    
    * login&logout test cases
    
    * Delete __pycache__ directory
    
    * Delete static directory
    
    * Delete templates directory
    
    * Delete Dockerfile
    
    * Delete LICENSE
    
    * Delete apiCall.py
    
    * Delete emailHandler.py
    
    * Delete main.py
    
    * Delete hasher.py
    
    * Delete tasks_db.py
    
    * added 2 most important use cases
    
    verbosely commented
    
    * imported views.py routes into app.py
    
    integrate attendance blueprint* into main app
    
    *views.py is a blueprint that defines routes outside the main module (app.py)
    
    ---------
    
    Co-authored-by: hRhoa <henryar2024@gmail.com>
    Co-authored-by: henhen10 <sh10079979@gmail.com>

---
+++ app.py
@@ -0,0 +1,111 @@
+import re, secrets
+from flask import Flask, request, redirect, url_for, render_template_string, session
+from flask_bcrypt import Bcrypt
+from datetime import timedelta
+from views import attendance_bp
+
+app = Flask(__name__)
+app.secret_key = secrets.token_hex(32) # Change this before the final version, will log everyone out each time the server restarts
+app.permanent_session_lifetime = timedelta(days=7)
+
+
+app.register_blueprint(attendance_bp)
+
+bcrypt = Bcrypt(app)
+
+users_db = {
+    "dal123456": {
+        "password": bcrypt.generate_password_hash("password1234*").decode('utf-8'),
+        "is_instructor": False
+    },
+    "proftest": {
+        "password": bcrypt.generate_password_hash("profpass1*").decode('utf-8'),
+        "is_instructor": True
+    }
+}
+
+# check if valid netid
+# very old netids can be only characters (will likely only be applicable for professors who have been here a while)
+# newer netids will be 3 letters followed by 6 digits
+# all netids will be between 5-9 characters
+def valid_netid(netid):
+    return re.fullmatch(r"([a-z]{5,9}|[a-z]{3}[0-9]{6})", netid) is not None
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # check if already logged in
+    if "netid" in session:
+        if session.get("is_instructor"):
+            return redirect(url_for("instructor_dashboard"))
+        else:
+            return redirect(url_for("student_dashboard"))
+    # generic log in
+    if request.method == 'POST':
+        netid = request.form['netid'].strip()
+        password = request.form['password'].strip()
+
+        # validate netid is correct format
+        if not valid_netid(netid):
+            return "Invalid NetID format. Please ensure your NetID is all lowercase."
+
+        # validate user exists
+        if netid not in users_db:
+            return "User not found. Please try again."
+
+        # check password
+        user = users_db[netid]
+        hashed_password = user["password"]
+        if bcrypt.check_password_hash(hashed_password, password):
+            session.permanent = True
+            session["netid"] = netid
+            session["is_instructor"] = user["is_instructor"]
+
+            if user["is_instructor"]:
+                return redirect(url_for("instructor_dashboard"))
+            else:
+                return redirect(url_for('student_dashboard'))
+        else:
+            return "Incorrect password. Please try again."
+
+    return render_template_string("""
+        <form method="post">
+            NetID: <input name="netid" type="text"><br>
+            Password: <input name = "password" type="password"><br>
+            <input type="submit">
+        </form>
+    """)
+
+@app.route('/instructor_dashboard')
+def instructor_dashboard():
+        if "netid" not in session:
+            return redirect(url_for("login"))
+
+        if not session.get("is_instructor"):
+            return "Access denied."
+
+        return f"""
+            Instructor Dashboard: {session['netid']}
+            <form action="/logout" method="post">
+                <button type="submit">Logout</button>
+            </form>
+        """
+
+@app.route('/student_dashboard')
+def student_dashboard():
+    if "netid" not in session:
+        return redirect(url_for("login"))
+
+    if session.get("is_instructor"):
+        return "Access denied."
+
+    return f"""
+        Student Dashboard: {session['netid']}
+        <form action="/logout" method="post">
+                <button type="submit">Logout</button>
+        </form>
+    """
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
